15480f4111210a39e1f40944e7a37740107720d7
Richard Möhn <richard.moehn@posteo.de>
2016-09-14 17:03:43 +0900

    Writer until-converged trainer (untested)

---
+++ hiora_cartpole/driver.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import functools
+import itertools
+from itertools import imap, islice, izip, dropwhile
+import operator
+
+import numpy as np
+import pyrsistent
+
+def iterate(f, x):
+    while True:
+        yield x
+        x = f(x)
+
+#### Some iterators
+
+# Credits: https://docs.python.org/2/library/itertools.html
+
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    return itertools.izip_longest(fillvalue=fillvalue, *args)
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return izip(a, b)
+
+
+# Timestep that the driver sees
+DTimestep = pyrsistent.immutable(
+                'observation, reward, done, experience, action',
+                name='DTimestep')
+
+
+def make_next_dtimestep(env, think):
+    def next_dtimestep_inner(dtimestep):
+        if dtimestep.done:
+            return dtimestep.set(reward=0, action=None)
+
+        new_experience, new_action = think(dtimestep.experience,
+                                           dtimestep.observation,
+                                           dtimestep.reward)
+        new_observation, new_reward, new_done, _ = env.step(new_action)
+
+        return DTimestep(new_observation, new_reward, new_done,
+                         new_experience, new_action)
+
+    return next_dtimestep_inner
+
+
+def make_train_and_prep(env, next_dtimestep, wrapup):
+    def train_and_prep_inner(first_dtimestep, max_steps=-1):
+        enumerator  = itertools.count(0) if max_steps < 0 else xrange(max_steps)
+        dtimestep   = first_dtimestep
+        n_dtimestep = 0
+        for n_dtimestep in enumerator:
+            dtimestep = next_dtimestep(dtimestep)
+
+            if dtimestep.done:
+                break
+
+        experience = wrapup(dtimestep.experience,
+                            dtimestep.observation,
+                            dtimestep.reward)
+
+        return n_dtimestep + 1, \
+               DTimestep(env.reset(), 0, False, experience, None)
+
+    return train_and_prep_inner
+
+
+# pylint: disable=too-many-arguments, too-many-locals
+def train_until_converged(env, train_and_prep, init_experience,
+        max_steps, max_episodes, avg_window, max_diff):
+    first_dtimestep   = DTimestep(env.reset(), 0, False, init_experience, None)
+
+    train_and_prep_ms = functools.partial(train_and_prep, max_steps=max_steps)
+    cnts_dtimesteps   = iterate(lambda (_, dt): train_and_prep_ms(dt),
+                                (0, first_dtimestep))
+
+    numbered_cnts_dtimesteps = izip(cnts_dtimesteps, itertools.count(0))
+    limited_cnts_dtimesteps  = islice(cnts_dtimesteps, max_episodes - 1)
+    cnts                     = imap(lambda (c, _): c, limited_cnts_dtimesteps)
+
+    # The underlying iterator is cnts_dtimesteps. The following will implicitly
+    # consume cnts_timesteps until the difference between the averages of
+    # adjacent groups falls below max_diff. Mutability is crazy, isn't it?
+    next(
+        dropwhile(lambda avg: avg >= max_diff,
+            imap(operator.sub,
+                pairwise(
+                    imap(np.average,
+                         grouper(cnts, avg_window)
+                    )
+                )
+            )
+        ),
+        None
+    )
+
+    nr, cnt_dtimestep = next(numbered_cnts_dtimesteps)
+
+    return nr, cnt_dtimestep[0], cnt_dtimestep[1]
+
+
+
+#def train(n_episodes, env, think, init_experience):
+#    dtimestep = DTimestep(env.reset(), 0, init_experience, None)
+#    for _ in xrange(n_episodes):
+#        dtimestep = train_and_prep(dtimestep)
+#
+#    return V_from_Q(dtimestep.experience.Q)
