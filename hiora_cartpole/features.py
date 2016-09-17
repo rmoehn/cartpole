3343d5216f5441a04acdf3cff070f2371cf28ae6
Richard Möhn <richard.moehn@posteo.de>
2016-09-17 17:41:16 +0900

    Make tilecoding general

--- hiora_cartpole/features.py
+++ hiora_cartpole/features.py
@@ -7,7 +7,6 @@ import operator
 import numpy as np
 from scipy import sparse
 
-import gym
 from tilecoding import representation
 
 class TileCodingFeatureVec(object):
@@ -23,44 +22,45 @@ class TileCodingFeatureVec(object):
         vec[self.feature_vec.indices] += 1.0
 
 
-def make_feature_vec(ndivs, ntilings):
+    def alphabounds_diffdot(self, prev, elig):
+        return self.dot(elig) - prev.dot(elig)
+
+
+def make_feature_vec(state_ranges, n_acts, ndivs, ntilings):
     """
-
-
-    Note that this implementation works for the cart-pole environment only.
 
     Arguments:
         ndivs    – (n_dim), number of divisions (in each tiling) for each of the
                    four input dimensions
         ntilings - int, number of tilings
     """
+    n_dim = state_ranges.shape[1]
     # For each dimension, one row of offsets per tiling.
-    unscaled_offsets = np.random.random_sample(4 * ntilings)\
-                           .reshape((4, ntilings))
+    # Note: if you use np.random.random, you don't need the reshape.
+    unscaled_offsets = np.random.random_sample(n_dim * ntilings)\
+                           .reshape((n_dim, ntilings))
     # unscaled_offsets are from interval [0, 1[. Make them smaller than the
     # width of the divisions in every dimension.
-    offsets = -1.0/np.array(ndivs) * unscaled_offsets
-
-    cartpole = gym.make('CartPole-v0')
+    offsets = -1.0/np.array(ndivs)[None,:].transpose() * unscaled_offsets
 
     state_tc = representation.TileCoding(
-                   input_indices=[[0, 1, 2, 3]],
-                   ntiles=ndivs + [2],
+                   input_indices=[range(n_dim)],
+                   ntiles=ndivs,
                    ntilings=[ntilings],
-                   state_range=np.array([cartpole.observation_space.low,
-                                         cartpole.observation_space.high]),
+                   state_range=state_ranges,
                    offsets=np.array([offsets]),
                    hashing=None,
                    bias_term=False)
 
     state_inds_to_sparse = representation.IndexToBinarySparse(state_tc)
 
-    n_weights = 2 * ntilings * reduce(operator.mul, ndivs)
+    n_weights_per_act = ntilings * reduce(operator.mul, ndivs)
+    n_weights         = n_acts * n_weights_per_act
 
 
     def feature_vec_inner(state, action):
         state_fv = state_inds_to_sparse(state) # fv … feature vector
-        # Note: Normally it would look like this:
+        # Note: Normally it would look like this (n_acts == 2):
         #
         #  tiling   0       1       2
         #  action = 0   1   0   1   0   1
@@ -74,15 +74,14 @@ def make_feature_vec(ndivs, ntilings):
         #
         # For the scalar product and the weights, this shouldn't make a
         # difference, though.
-        if action:
-            return TileCodingFeatureVec(
-                       sparse.hstack(
-                           [state_fv, sparse.csr_matrix(state_fv.shape)],
-                           format='csr'))
-        else:
-            return TileCodingFeatureVec(
-                       sparse.hstack(
-                           [sparse.csr_matrix(state_fv.shape), state_fv],
-                           format='csr'))
+
+        n_after    = n_acts - action - 1
+        n_before   = action
+        return TileCodingFeatureVec(
+                   sparse.hstack(
+                       [sparse.csr_matrix((1, n_before * n_weights_per_act)),
+                        state_fv,
+                        sparse.csr_matrix((1, n_after * n_weights_per_act))],
+                       format='csr'))
 
     return n_weights, feature_vec_inner
