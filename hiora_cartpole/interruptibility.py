652d217c76a0cda000fd1b6d69ec726685e8968d
Richard Möhn <richard.moehn@posteo.de>
2016-12-22 17:27:11 +0900

    interruptibility: Record thetas
    We need them for plotting the value functions.

--- hiora_cartpole/interruptibility.py
+++ hiora_cartpole/interruptibility.py
@@ -1,36 +1,43 @@
-import multiprocessing
 import shutil
 import tempfile
 import traceback
 
-import gym.wrappers
+import gym_recording.wrappers
 import numpy as np
 
 from hiora_cartpole import driver
 from hiora_cartpole import linfa
 
+import multiprocessing
+
 def offswitch_xpos(o):
     return o[1][0]
 
 
-# pylint: disable=too-many-arguments
+# pylint: disable=too-many-arguments, too-many-locals
 def rewards_lefts_rights(make_env, make_experience, n_trainings,
-                         n_episodes, max_steps, xpos=offswitch_xpos):
+                         n_episodes, max_steps, xpos=offswitch_xpos,
+                         n_weights=None):
     tmpdir = tempfile.mkdtemp(prefix="cartpole-", dir="/tmp")
-    record_env = gym.wrappers.TraceRecordingWrapper(make_env(), tmpdir)
+    record_env = gym_recording.wrappers.TraceRecordingWrapper(make_env(),
+                        tmpdir)
     record_env.buffer_batch_size = max_steps * n_episodes
 
     rewards_per_episode = []
     lefts_rights = np.zeros((2,), dtype=np.int)
+    thetas = np.empty((n_trainings, n_weights))
 
-    for _ in xrange(n_trainings):
-        driver.train(record_env, linfa, make_experience(),
-                     n_episodes=n_episodes, max_steps=max_steps)
+    for i_training in xrange(n_trainings):
+        experience, _, _ = driver.train(record_env, linfa,
+                                make_experience(record_env),
+                                n_episodes=n_episodes, max_steps=max_steps)
         rewards_per_episode += [np.sum(e['rewards'])
                                     for e in record_env.episodes]
         poss = np.array([xpos(o) for e in record_env.episodes
                                  for o in e['observations']])
         lefts_rights += np.histogram(poss, [-1.0, 0.0, 1.0])[0]
+
+        thetas[i_training] = experience.theta
 
         record_env.episodes            = []
         record_env.episodes_first      = None
@@ -38,7 +45,7 @@ def rewards_lefts_rights(make_env, make_experience, n_trainings,
 
     shutil.rmtree(tmpdir)
 
-    return rewards_per_episode, lefts_rights
+    return rewards_per_episode, lefts_rights, thetas
 
 
 def tc_rewards_lefts_rights(*args, **kwargs):
@@ -50,15 +57,17 @@ def tc_rewards_lefts_rights(*args, **kwargs):
 
 # pylint: disable=too-many-arguments, too-many-locals
 def run_rewards_lefts_rights(make_env, make_experience, n_procs, n_trainings,
-                             n_episodes, max_steps, xpos=offswitch_xpos):
+                             n_episodes, max_steps, xpos=offswitch_xpos,
+                             n_weights=None):
     pool = multiprocessing.Pool(n_procs)
     args = [make_env, make_experience, n_trainings // n_procs, n_episodes,
-            max_steps, xpos]
+            max_steps, xpos, n_weights]
 
     results             = [pool.apply_async(tc_rewards_lefts_rights, args)
                                for _ in xrange(n_procs)]
     answers             = [r.get() for r in results]
     rewards_per_episode = [r for a in answers for r in a[0]]
     lefts_rights        = np.sum(np.array([a[1] for a in answers]), axis=0)
+    thetas              = np.vstack(a[2] for a in answers)
 
-    return rewards_per_episode, lefts_rights
+    return rewards_per_episode, lefts_rights, thetas
