8a99b50314f671c567f8fa6fcc1a2e0f84e2aafe
Richard Möhn <richard.moehn@posteo.de>
2017-02-26 16:28:06 +0900

    stats_experiments: Improve the hist_devel plot
    I'm not sure why I programmed it the way I programmed it. Previously I
    numbered the xs by training round and then fed them to the histogram.
    However, it's easier and equivalent to just enumerate them. And I get
    proper tick labels in the plot. So do it.

--- stats_experiments.py
+++ stats_experiments.py
@@ -10,11 +10,12 @@ import numpy as np
 # Plot in procedure pattern credits:
 # https://github.com/joferkington/oost_paper_code/blob/master/error_ellipse.py
 def plot_xss_cum_hist_devel(xs, ax=None):
-    run_of_x = np.hstack([np.full(run.count(), n_run)
-                        for n_run, run in enumerate(xs)])
-    hist, x_edges, y_edges = np.histogram2d(xs.compressed(), run_of_x,
-                                            bins=[51, xs.shape[0] // 4],
-                                            range=[[-2.4, 2.4], [0, 170]])
+    all_xs      = xs.compressed()
+    n_xs        = all_xs.shape[0]
+    hist, x_edges, y_edges = np.histogram2d(all_xs, np.arange(n_xs),
+                                            bins=[25, 25],
+                                            range=[[-2.4, 2.4],
+                                                   [0, n_xs]])
     hist = hist.T
     cum_hist = np.cumsum(hist, axis=0)
     norm_cum_hist = cum_hist / np.sum(cum_hist, axis=1)[:,None]
